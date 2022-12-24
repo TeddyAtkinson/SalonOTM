e5ddb6cdff5709aba73de51822311c35d2865f18
teddyatkinson <tedddy.atkinson@gmail.com>
2022-12-23 16:34:55 -0800

    Stylist view refactoring and correction

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -1,10 +1,11 @@
+using System.Collections.Generic;
+
 namespace Salon.Models
 {
   public class Stylist
   {
     public int StylistId { get; set; }
     public string Name { get; set; }
-    public int ClientId { get; set; }
-    public Client Client { get; set; }
+    public List<Client> Clients { get; set; }
   }
 }
