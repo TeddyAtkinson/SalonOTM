94490e6660f9c6c529672bb52e4a9fa6d7d9462b
teddyatkinson <tedddy.atkinson@gmail.com>
2023-01-09 09:18:17 -0800

    Delete method for client

--- HairSalon/Controllers/ClientsController.cs
+++ HairSalon/Controllers/ClientsController.cs
@@ -40,5 +40,11 @@ namespace HairSalon.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+      return View(thisClient);
+    }
   }
 }
