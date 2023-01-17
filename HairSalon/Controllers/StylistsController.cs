72e1b2e495e5f9d3434c939a78c0f0b90c036e7b
teddyatkinson <tedddy.atkinson@gmail.com>
2023-01-17 04:05:02 -0800

    Add sql database

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -40,7 +40,7 @@ namespace Salon.Controllers
       return View(thisStylist);
     }
 
-    [HttpPost]
+    [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
       Stylist thisStylist = _db.Stylists.FirstOrDefault(category => category.StylistId == id);
